ab9920854ccee1968601e93ece7a5a5321f0f043
logithaadithya-s <logithjee@gmail.com>
2025-02-14 11:13:32 +0530

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,352 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.mainstore');
+    const dropdownContent = document.querySelector('.store');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.mainmac');
+    const dropdownContent = document.querySelector('.mac');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.ipadstr');
+    const dropdownContent = document.querySelector('.ipad');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.mainiphn');
+    const dropdownContent = document.querySelector('.iphn');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.applewth');
+    const dropdownContent = document.querySelector('.applewth1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.airpd');
+    const dropdownContent = document.querySelector('.airpd1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.tvhome');
+    const dropdownContent = document.querySelector('.tvhome1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 200);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.entert');
+    const dropdownContent = document.querySelector('.entert1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 200);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.Accessories');
+    const dropdownContent = document.querySelector('.Accessories1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 200);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.store');
+    const dropdownContent = document.querySelector('.store1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 200);
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.querySelector('.supports');
+    const dropdownContent = document.querySelector('.supports1');
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+        dropdownContent.style.display = 'block';
+        dropdownContent.style.opacity = '1';
+        dropdownContent.style.visibility = 'visible';
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'none';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 20);
+    });
+
+    dropdownContent.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId);
+    });
+
+    dropdownContent.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            dropdownContent.style.display = 'inline-block';
+            dropdownContent.style.opacity = '0';
+            dropdownContent.style.visibility = 'hidden';
+        }, 200);
+    });
+});
